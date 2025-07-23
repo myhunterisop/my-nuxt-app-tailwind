2357b2b6ef8b3f1a0af7d7ce069107507c539436
borovkov.yu <borovkov.yu@promail366.com>
2025-07-24 01:58:16 +0300

    fix init

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  builder: 'vite',
+  css: ['~/assets/css/main.css'],
+  vite: {
+    resolve: {
+      alias: {
+        '@images': '/public/images',
+      },
+    },
+    plugins: [
+      tailwindcss(),
+      {
+        name: 'console-on-build',
+        buildStart() {
+          console.log('🔥 Vite is building from nuxt.config.ts')
+        },
+      },
+    ],
+  },
+})
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app.vue',
+    './app/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './pages/**/*.{vue,js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
